1639de22ab78347a3e48a5894b5bc9546493af67
Smriti5 <48525729+Smriti5@users.noreply.github.com>
2019-06-10 20:00:22 +0530

    seventh commit

---
+++ age.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+name=raw_input("Enter your name:")
+age=int(input("Enter age:"))
+current_year=int(input("Enter current year:"))
+d=current_year-age
+e=d+95
+print("The year in which you will be 95 is :")
+print(e)
